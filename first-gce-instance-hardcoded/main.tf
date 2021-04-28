8ebbac44ece10e4248bc2d7c268c5571a131649c
root <root@linux-1.us-central1-a.c.revanth-gcp-2020.internal>
2021-04-28 17:40:14 +0000

    adding first instance creation - hardcoded values

---
+++ first-gce-instance-hardcoded/main.tf
@@ -0,0 +1,31 @@
+resource "google_compute_instance" "vm_instance" {
+	name  = "my-tf-instance"
+	machine_type = "f1-micro"
+	zone = "europe-west2-b"
+
+	boot_disk {
+		initialize_params {
+			# image can be a particular  machine image or a fully qualified image-family name
+			image = "debian-cloud/debian-9"
+			# image = "projects/<PROJECT_NAME>/global/images/family/<IMAGE_NAME>"
+		}
+		# CMEK (keys managed in Google cloud KMS) 
+		# kms_key_self_link = "projects/<PROJECT_NAME>/locations/<LOCATION>/keyRings/<EXISTING_PROJ_NAME>/cryptoKeys/<KEY_NAME>"
+	}
+
+	network_interface {
+		# subnetwork = "projects/<PROJECT_NAME>/regions/<REGION_OF_SUBNET>/<SUBNET_NAME>"
+		# network_ip = "10.0.0.1" -- Static IP (Private IP address)
+	}
+	scheduling {
+		on_host_maintenance = "MIGRATE"
+	}
+	
+	metadata_startup_script_url = "gs://<BUCKET_NAME>/path/to/file/filename.sh"
+
+  	service_account {
+    		email  = "<SERVICE_ACCOUNT_NAME>@<PROJECT_NAME>.iam.gserviceaccount.com"
+    		scopes = ["cloud-platform"]
+  	}
+	tags = ["allow-nginx-ingress"]
+}
---
+++ first-gce-instance-hardcoded/provider.tf
@@ -0,0 +1,7 @@
+
+provider "google" {
+	project = "revanth-gcp-2020"
+	region = "us-central1"
+	zone = "us-central1-a"
+	credentials = "/path/to/file/SA_key.json"
+}
