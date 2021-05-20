0ce2fdca797419df4185cfc1cc804a7b42b348a5
suren673 <54912467+suren673@users.noreply.github.com>
2021-05-20 15:46:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,31 @@
-resource "google_compute_instance" "vm_instance" {
-	name  = "final-time-1"
-	machine_type = "f1-micro"
-	zone = "us-central1-a"
 
-	boot_disk {
-		initialize_params {
-			# image can be a particular  machine image or a fully qualified image-family
-			image = "debian-cloud/debian-9"
-			
-		}
-		
-	}
 
-	network_interface {
-		# subnetwork = "projects/<PROJECT_NAME>/regions/<REGION_OF_SUBNET>/<SUBNET_NAME>"
-		# network_ip = "10.0.0.1" -- Static IP (Private IP address)
-		network = "default"
-    		access_config {
-    		}
-	}
-	scheduling {
-		on_host_maintenance = "MIGRATE"
-	}
-	
-	#metadata_startup_script_url = "gs://<BUCKET_NAME>/path/to/file/filename.sh"
+provider "google" {
+ 
 
-  	service_account {
-    		email  = "terraform-deployer@revanth-gcp-2020.iam.gserviceaccount.com"
-    		scopes = ["cloud-platform"]
-  	}
-	tags = ["allow-nginx-ingress"]
+  //credentials = var.credentials
+
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
 }
 
-output "gce_ext_ip" {
-	value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+resource "google_compute_instance" "default" {
+  name= "test"
+  machine_type="e2-micro"
+
+  boot_disk {
+      initialize_params{
+          image="debian-cloud/debian-9"
+      }
+  }
+network_interface {
+    network="default"
+
+    access_config{
+
+    }
+}
+
+
 }
