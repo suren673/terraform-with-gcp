64a8e91339134cc74cc3b4ec06c1705bcc7c6226
suren673 <54912467+suren673@users.noreply.github.com>
2021-05-20 19:59:37 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "gcp_project" {
+  type        = string
+  description = "GCP project"
+  //default = "crucial-strata-311604"
+}
+
+variable "gcp_zone" {
+  type        = string
+  description = "GCP zone"
+  //default = "us-central1-a"
+}
+
+variable "gcp_region" {
+  type        = string
+  description = "GCP region"
+  //default =  "us-central1"
+}
+
+variable "gcp_instancename" {
+  type        = string
+  description = "vm instance name"
+  //default =  "variablevminstance"
+}
+
+variable "gcp_machinetype" {
+  type        = string
+  description = "vm machine name"
+  //default =  "e2-micro"
+}
+
+variabe "gcp_auth_file" {
+  type        = string
+  description = "authentication file"
+}
